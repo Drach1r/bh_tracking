d2e111f45345945109dd258faf24269b591b1a97
Drach1r <118975464+Drach1r@users.noreply.github.com>
2024-03-14 10:51:51 +0800

    Add files via upload

---
+++ includes/boot.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Manila");
+$dateNow = date('Y-m-d H:i:s');
+
+$pdo = null;
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');  // Replace with your MySQL username
+define('DB_PASSWORD', '');  // Replace with your MySQL password
+define('DB_NAME', 'checklist');
+define('APP_ROOT', 'http://localhost/bh_tracking/');
+
+try {
+	$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	// Log the error instead of displaying it directly on the page in a production environment
+	error_log('Connection Failed: ' . $e->getMessage());
+	die('Connection Failed');
+}
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php
+require_once('boot.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Boarding House Tracking</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="<?php echo Navigation::BaseNav('path/to/file') ?>">
+    <style type="text/css">
+
+    </style>
+
+<body>
