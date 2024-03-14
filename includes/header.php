52c9d8d90cac110871e52fc1236ea98b713a53d8
Drach1r <richardmarc.paredes1@gmail.com>
2024-03-14 11:12:04 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,102 @@
 <?php
 require_once('boot.php');
+
+if (!isset($_SESSION['user_id'])) {
+
+    header("Location: login.php");
+    exit();
+}
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <title>Boarding House Tracking</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="<?php echo Navigation::BaseNav('path/to/file') ?>">
-    <style type="text/css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BH Tracking</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="resources/fontawesome-free-6.5.1-web/css/fontawesome.css">
 
-    </style>
+    <link rel="stylesheet" type="text/css" href="resources/fontawesome-free-6.5.1-web/css/solid.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="icon" type="image/x-icon" href="resources/img/iloilo.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/compressorjs@1.1.5/dist/compressor.min.js"></script>
+
+
+
+</head>
+<style type="text/css">
+    table tbody tr:hover {
+        cursor: pointer;
+    }
+
+    .normalTr:hover {
+        cursor: default;
+    }
+</style>
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container-fluid">
+            <a href="index.php"><img src="resources/img/iloilo.png" alt="" height="80"></a>
+            <a class="navbar-brand <?php echo basename($_SERVER['PHP_SELF']) == 'csv.php' ? 'active' : ''; ?>" href="csv.php" style="color: <?php echo basename($_SERVER['PHP_SELF']) == 'csv.php' ? 'blue' : 'white'; ?>;  margin-left: 30px;">Import CSV</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+
+                    </li>
+                </ul>
+                <form class="d-flex" role="">
+                    <button type="submit" class="btn btn-danger"><a class="dropdown-item" href="logout.php">
+                            Logout
+                        </a>
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    </nav>
+
+</header>
 
 <body>
+
+    <?php
+    if (isset($_SESSION['error'])) {
+        echo "<div class='alert alert-danger text-center'>
+            <i class='fas fa-exclamation-triangle'></i> " . $_SESSION['error'] . "
+          </div>";
+
+        // unset error
+        unset($_SESSION['error']);
+    }
+
+    if (isset($_SESSION['success'])) {
+        echo "<div class='alert alert-success text-center'>
+            <i class='fas fa-check-circle'></i> " . $_SESSION['success'] . "
+          </div>";
+
+        // unset success
+        unset($_SESSION['success']);
+    }
+    ?>
+    <br>
