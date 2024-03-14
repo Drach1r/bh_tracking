69c18f70e03ca478eac4d971580d3b0c7888cc7b
Drach1r <richardmarc.paredes1@gmail.com>
2024-03-14 13:26:28 +0800

    Update csv.php

--- csv.php
+++ csv.php
@@ -2,61 +2,13 @@
 include 'includes/header.php';
 ?>
 
-<h3 style="margin-left: 30px;">Records List </h3>
 
-<form action="import.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" accept=".csv">
-        <button type="submit" name="submit">Upload CSV</button>
-    </form>
 
-<div class="container">
-    <br>
-    <div class="card-body">
-        <div class="card-title-body">
-            <script>
-                $(document).ready(function() {
-                    var table = $('#recordstable').DataTable({
-                        'pageLength': 10,
-                        'scrollY': '40vh',
-                        columnDefs: [{
-                                width: '10%',
-                                targets: 1
-                            },
-                            {
-                                width: '10%',
-                                targets: 3
-                            },
-                        ]
-                    });
-                });
-            </script>
+<article class="content items-list-page">
+    <div class="title-search-block">
+        <div class="title-block">
+            <div class="row form-group">
 
-<<<<<<< Updated upstream
-            <table class="table table-bordered table-hover w-100" id="recordstable">
-                <thead class="table-dark">
-                    <tr>
-                        <th class="text-center">Account Number</th>
-                        <th class="text-center">Name of Establishment</th>
-                        <th class="text-center">Full Name</th>
-                        <th class="text-center">Address</th>
-                        <th class="text-center">City Municipality</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $database = "bh_tracking";
-
-                    try {
-                        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                        $sql = "SELECT id, account_number, establishment_name, first_name, middle_name, last_name, bh_address, bh_municipality FROM boarding_house_tracking";
-                        $stmt = $conn->prepare($sql);
-                        $stmt->execute();
-=======
                 <div class="col-sm-6">
                     <br>
                     <h3 class="title" style="margin-left: 50px;">Records List </h3>
@@ -74,32 +26,11 @@ include 'includes/header.php';
                             </div>
                         </div>
                     </form>
->>>>>>> Stashed changes
 
-                        if ($stmt->rowCount() > 0) {
-                            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                echo "<tr>";
-                                echo "<td class='text-center'>" . $row['account_number'] . "</td>";
-                                echo "<td class='text-center'>" . $row['establishment_name'] . "</td>";
-                                echo "<td class='text-center'>" . $row['first_name'] . " " . $row['middle_name'] . " " . $row['last_name'] . "</td>";
-                                echo "<td class='text-center'>" . $row['bh_address'] . "</td>";
-                                echo "<td class='text-center'>" . $row['bh_municipality'] . "</td>";
-                                echo "</tr>";
-                            }
-                        } else {
-                            echo "<tr><td colspan='5' class='text-center'>No records found</td></tr>";
-                        }
-                    } catch (PDOException $e) {
-                        echo "Connection failed: " . $e->getMessage();
-                    }
-                    ?>
-                </tbody>
-            </table>
+                </div>
+            </div>
         </div>
     </div>
-<<<<<<< Updated upstream
-</div>
-=======
     <br>
 
 
@@ -154,13 +85,13 @@ include 'includes/header.php';
                             <table class="table table-bordered table-hover w-100" id="recordstable">
                                 <thead class="table-dark">
                                     <tr>
-                                      
+
                                         <th class="text-center">Account Number</th>
                                         <th class="text-center">Name of Establishment</th>
                                         <th class="text-center">Full Name</th>
                                         <th class="text-center">Address</th>
                                         <th class="text-center">City Municipality</th>
-                                        
+
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -189,7 +120,8 @@ include 'includes/header.php';
                                                 echo "<tr>";
                                                 echo "<td class='text-center'>" . $row['account_number'] . "</td>";
                                                 echo "<td class='text-center'>" . $row['establishment_name'] . "</td>";
-                                                echo "<td class='text-center'>" . $row['first_name'. 'middle_name'. 'last_name'.'suffix'] . "</td>";
+                                                echo "<td class='text-center'>" . $row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name'] . ' ' . $row['suffix'] . "</td>";
+
                                                 echo "<td class='text-center'>" . $row['bh_address'] . "</td>";
                                                 echo "<td class='text-center'>" . $row['bh_municipality'] . "</td>";
                                                 echo "</tr>";
@@ -213,7 +145,6 @@ include 'includes/header.php';
     </section>
 </article>
 
->>>>>>> Stashed changes
 
 <?php
 include 'includes/footer.php';
