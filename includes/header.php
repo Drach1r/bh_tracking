444db2485921124510dbc9a83a34525ce50f0d50
Drach1r <richardmarc.paredes1@gmail.com>
2024-03-20 16:40:13 +0800

    33

--- includes/header.php
+++ includes/header.php
@@ -58,7 +58,7 @@ if (!isset($_SESSION['user_id'])) {
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
             <a href="records.php"><img src="resources/img/iloilo.png" alt="" height="80"></a>
-<h5 style= "color: white;">Boarding House Tracking Information Management Application</h5>
+            <h5 style="color: white; margin-left: 20px; ">Boarding House Tracking Information Management Application</h5>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
