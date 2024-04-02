c36657134847dc3dd68698e0212b2fa62ee63dde
Drach1r <richardmarc.paredes1@gmail.com>
2024-04-02 13:59:59 +0800

    q

--- map.php
+++ map.php
@@ -1,118 +1,118 @@
 <?php include 'includes/header.php'; ?>
 
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <style>
-        #map {
-            height: 600px;
-        }
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+<style>
+    #map {
+        height: 600px;
+    }
 
-        .popup-content {
-            max-height: 300px;
-            max-width: 300px;
-            overflow-y: auto;
-            padding: 10px;
-            font-size: 14px;
-            list-style: none;
-            padding-left: 0;
-        }
+    .popup-content {
+        max-height: 300px;
+        max-width: 300px;
+        overflow-y: auto;
+        padding: 10px;
+        font-size: 14px;
+        list-style: none;
+        padding-left: 0;
+    }
 
-        
-        .popup-content h4 {
-            margin-bottom: 0px;
-            font-weight: bold;
-         
-        }
 
-        .popup-content li {
-            margin-bottom: 5px;
-            list-style: none;
-           
-        }
-    </style>
-    <div id="map"></div>
+    .popup-content h4 {
+        margin-bottom: 0px;
+        font-weight: bold;
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script>
-        $(document).ready(function () {
-            var map = L.map('map').setView([10.7202, 122.5621], 13); 
+    }
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+    .popup-content li {
+        margin-bottom: 5px;
+        list-style: none;
 
-            $.ajax({
-                url: 'resources/dr/pins.php',
-                type: 'GET',
-                dataType: 'json',
-                success: function (data) {
-                    data.forEach(function (item) {
-                        var latitude = parseFloat(item.bh_latitude);
-                        var longitude = parseFloat(item.bh_longitude);
-                        var accountNumber = String(item.account_number);
-                        var establishmentName = String(item.establishment_name);
-                        var firstName = String(item.first_name);
-                        var middleName = String(item.middle_name);
-                        var lastName = String(item.last_name);
-                        var address = String(item.bh_address);
-                        var municipality = String(item.bh_municipality);
-                        var district = String(item.bh_district);
-                        var barangay = String(item.bh_barangay);
-                        var province = String(item.bh_province);
-                        var controlNumber = String(item.bh_control_no);
-                        var orNumber = String(item.bh_or_num);
-                        var dateIssued = String(item.date_issued);
-                        var amountPaid = parseFloat(item.amount_paid);
-                        var bpn = String(item.bh_bpn);
-                        var mp = String(item.bh_mp);
-                        var datePaid = String(item.date_paid);
-                        var periodCover = String(item.bh_period_cover);
-                        var complaint = String(item.bh_complaint);
-                        var constructionKind = String(item.bh_construction_kind);
-                        var specify = String(item.bh_specify);
-                        var bhClass = String(item.bh_class);
-                        var bhRoom = String(item.bh_room);
-                        var occupants = parseInt(item.bh_occupants) || 0; // Default to 0 if value is missing or invalid
-                        var overcrowded = parseInt(item.bh_overcrowded) || 0;
-                        var ratesCharge = item.bh_rates_charge ? String(item.bh_rates_charge) : '';
-                        var ratesChargeOther = item.bh_ratescharge_other ? String(item.bh_ratescharge_other) : '';
-                        var rate = parseFloat(item.bh_rate) || 0.0; // Default to 0.0 if value is missing or invalid
-                        var waterSource = String(item.bh_water_source);
-                        var adequate = String(item.bh_adequate);
-                        var portable = String(item.bh_portable);
-                        var toilettype = String(item.bh_toilet_type);
-                        var toiletcond = String(item.bh_toilet_cond);
-                        var bathtype = String(item.bh_bath_type);
-                        var bathcond = String(item.bh_bath_cond);
-                        var crnum = String(item.bh_cr_num);
-                        var bathroomnum = String(item.bh_bathroom_num);
-                        var premisescond = String(item.bh_premises_cond);
-                        var garbagedisposal = String(item.bh_garbage_disposal);
-                        var garbageother = String(item.bh_garbage_other);
-                        var dps = String(item.bh_dps);
-                        var sewagedisposal = String(item.bh_sewage_disposal);
-                        var sewageother = String(item.bh_sewage_other);
-                        var sddps = String(item.bh_sd_dps);
-                        var rodentdisposal = String(item.bh_rodent_disposal);
-                        var rodentother = String(item.bh_rodent_other);
-                        var ventilation = String(item.light_ventilation);
-                        var artificial = String(item.natural_artificial);
-                        var extension = String(item.establishment_extension);
-                        var txt = String(item.specify_txt);
-                        var permit = String(item.with_permit);
-                        var remarks = String(item.bh_remarks);
-                        var required = String(item.office_required);
-                        var inspect = String(item.inspected_by);
-                        var acknowledge = String(item.acknowledge_by);
-                        var imageName = String(item.bh_image); 
-                        var imagePath = `resources/gallery/${imageName}`; 
+    }
+</style>
+<div id="map"></div>
 
-                        if (!isNaN(latitude) && !isNaN(longitude)) {
-                            var mapCenter = [latitude, longitude];
-                            var marker = L.marker(mapCenter).addTo(map);
-                            marker.bindPopup(
-                                `<div class="popup-content">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script>
+    $(document).ready(function() {
+        var map = L.map('map').setView([10.7202, 122.5621], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        $.ajax({
+            url: 'resources/dr/pins.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                data.forEach(function(item) {
+                    var latitude = parseFloat(item.bh_latitude);
+                    var longitude = parseFloat(item.bh_longitude);
+                    var accountNumber = String(item.account_number);
+                    var establishmentName = String(item.establishment_name);
+                    var firstName = String(item.first_name);
+                    var middleName = String(item.middle_name);
+                    var lastName = String(item.last_name);
+                    var address = String(item.bh_address);
+                    var municipality = String(item.bh_municipality);
+                    var district = String(item.district_name);
+                    var barangay = String(item.barangay_name);
+                    var province = String(item.bh_province);
+                    var controlNumber = String(item.bh_control_no);
+                    var orNumber = String(item.bh_or_num);
+                    var dateIssued = String(item.date_issued);
+                    var amountPaid = parseFloat(item.amount_paid);
+                    var bpn = String(item.bh_bpn);
+                    var mp = String(item.bh_mp);
+                    var datePaid = String(item.date_paid);
+                    var periodCover = String(item.bh_period_cover);
+                    var complaint = String(item.bh_complaint);
+                    var constructionKind = String(item.bh_construction_kind);
+                    var specify = String(item.bh_specify);
+                    var bhClass = String(item.bh_class);
+                    var bhRoom = String(item.bh_room);
+                    var occupants = parseInt(item.bh_occupants) || 0; // Default to 0 if value is missing or invalid
+                    var overcrowded = parseInt(item.bh_overcrowded) || 0;
+                    var ratesCharge = item.bh_rates_charge ? String(item.bh_rates_charge) : '';
+                    var ratesChargeOther = item.bh_ratescharge_other ? String(item.bh_ratescharge_other) : '';
+                    var rate = parseFloat(item.bh_rate) || 0.0; // Default to 0.0 if value is missing or invalid
+                    var waterSource = String(item.bh_water_source);
+                    var adequate = String(item.bh_adequate);
+                    var portable = String(item.bh_portable);
+                    var toilettype = String(item.bh_toilet_type);
+                    var toiletcond = String(item.bh_toilet_cond);
+                    var bathtype = String(item.bh_bath_type);
+                    var bathcond = String(item.bh_bath_cond);
+                    var crnum = String(item.bh_cr_num);
+                    var bathroomnum = String(item.bh_bathroom_num);
+                    var premisescond = String(item.bh_premises_cond);
+                    var garbagedisposal = String(item.bh_garbage_disposal);
+                    var garbageother = String(item.bh_garbage_other);
+                    var dps = String(item.bh_dps);
+                    var sewagedisposal = String(item.bh_sewage_disposal);
+                    var sewageother = String(item.bh_sewage_other);
+                    var sddps = String(item.bh_sd_dps);
+                    var rodentdisposal = String(item.bh_rodent_disposal);
+                    var rodentother = String(item.bh_rodent_other);
+                    var ventilation = String(item.light_ventilation);
+                    var artificial = String(item.natural_artificial);
+                    var extension = String(item.establishment_extension);
+                    var txt = String(item.specify_txt);
+                    var permit = String(item.with_permit);
+                    var remarks = String(item.bh_remarks);
+                    var required = String(item.office_required);
+                    var inspect = String(item.inspected_by);
+                    var acknowledge = String(item.acknowledge_by);
+                    var imageName = String(item.bh_image);
+                    var imagePath = `resources/gallery/${imageName}`;
+
+                    if (!isNaN(latitude) && !isNaN(longitude)) {
+                        var mapCenter = [latitude, longitude];
+                        var marker = L.marker(mapCenter).addTo(map);
+                        marker.bindPopup(
+                            `<div class="popup-content">
                                 <h4>${accountNumber}</h4>
                                 <ul>
                                     <li><b>Establishment Name:</b> ${establishmentName}</li>
@@ -167,18 +167,18 @@
                                     <li><img src="${imagePath}" alt="Boarding house image" style="max-width: 200px;"></li>
                                 </ul>
                             </div>`
-                            ).openPopup();
-                        } else {
-                            console.error('Invalid latitude or longitude:', item);
-                        }
-                    });
-                },
-                error: function (xhr, status, error) {
-                    console.error("Error fetching boarding house locations:", error);
-                }
-            });
+                        ).openPopup();
+                    } else {
+                        console.error('Invalid latitude or longitude:', item);
+                    }
+                });
+            },
+            error: function(xhr, status, error) {
+                console.error("Error fetching boarding house locations:", error);
+            }
         });
-    </script>
+    });
+</script>
 
 
 
--- resources/dr/pins.php
+++ resources/dr/pins.php
@@ -6,26 +6,30 @@ $dbname = "bh_tracking";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $sql = "SELECT bh_longitude, bh_latitude, bh_altitude, bh_precision, account_number, establishment_name,
-    first_name, middle_name, last_name, suffix, bh_address, bh_municipality, bh_district, bh_barangay, bh_province, bh_control_no, 
-    bh_or_num, date_issued, amount_paid,  bh_bpn, bh_mp, date_paid, bh_period_cover, bh_complaint, bh_construction_kind, bh_specify,
-    bh_class, bh_room, bh_occupants, bh_overcrowded, bh_rates_charge, bh_ratescharge_other, bh_rate, bh_water_source, bh_adequate, bh_portable,
-    bh_toilet_type, bh_toilet_cond, bh_bath_type, bh_bath_cond, bh_cr_num, bh_bathroom_num, bh_premises_cond, bh_garbage_disposal, bh_garbage_other, bh_sewage_disposal, 
-    bh_sewage_other, bh_rodent_disposal, bh_rodent_other, light_ventilation, natural_artificial, establishment_extension, specify_txt, with_permit, bh_remarks, office_required, inspected_by, 
-    acknowledge_by, bh_image FROM boarding_house_tracking";
+    $sql = "SELECT 
+    bht.*, 
+    bd.district_name AS district_name,
+    ba.barangay AS barangay_name
+FROM 
+    boarding_house_tracking bht
+LEFT JOIN 
+    bh_district bd ON bht.bh_district = bd.id
+LEFT JOIN 
+    bh_address ba ON bht.bh_barangay = ba.id;
+";
+
+
     $stmt = $pdo->query($sql);
-    
+
     $locations = array();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $locations[] = $row;
     }
-    
+
     header('Content-Type: application/json');
     echo json_encode($locations);
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-?>
