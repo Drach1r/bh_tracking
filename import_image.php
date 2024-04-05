04d4a4388f9a02ee9862bd8a1f58fa212bb557b0
Mary Hope Tabunlupa <maryhope.tabunlupa@wvsu.edu.ph>
2024-04-05 15:42:37 +0800

    new

--- import_image.php
+++ import_image.php
@@ -1,17 +1,17 @@
 <?php
 include 'includes/boot.php';
 
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "bh_tracking"; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bh_tracking";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_FILES['file'])) {
+        if (isset($_FILES['file']) && $_FILES['file']['name'][0] !== "") {
             $file_count = count($_FILES['file']['name']);
 
             for ($i = 0; $i < $file_count; $i++) {
@@ -26,11 +26,13 @@ try {
             }
 
             $_SESSION['success'] = "Files uploaded successfully.";
+        } else {
+            $_SESSION['error'] = "Please select at least one file.";
         }
     }
 
     header("Location: records.php");
-    exit(); 
+    exit();
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
